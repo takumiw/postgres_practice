45a7e5464e585d34783671994d84dacee25223c8
takumiw <w.tak.1229@gmail.com>
2021-04-08 02:19:24 +0900

    init

---
+++ src/test_2.sql
@@ -0,0 +1,7 @@
+/* データの挿入 */
+BEGIN TRANSACTION;
+INSERT INTO staff VALUES ('001', '山田太郎', 28);
+INSERT INTO staff VALUES ('002', '佐藤健二', 34);
+INSERT INTO staff VALUES ('003', '佐々木明里', 50);
+INSERT INTO staff VALUES ('004', '北野吾郎', 22);
+COMMIT;
